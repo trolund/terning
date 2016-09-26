fa3dc819836d6b081d92538a4380db0394f8b4ab
trolund <trolund@gmail.com>
2016-09-26 15:57:28 +0200

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -4,7 +4,7 @@ public class Dice {
 	// roll the die and return the value (1-6)
 	public int roll() {
 		
-		d3 = Math.floor(Math.random() * 5)
+		d3 = (int) Math.floor(Math.random() * 5)
 						           // 0-5 integer
 		return d3 + 1;                     // 1-6		
 	}
