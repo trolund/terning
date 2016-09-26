76149f07a8cb2542f8d95a18a95f79e3ab479606
KasperLeiszner <leisznern@gmail.com>
2016-09-26 16:14:23 +0200

    Added new test-case
    added TC02

--- DiceTest.java
+++ DiceTest.java
@@ -20,9 +20,49 @@ public class DiceTest
 			assertEquals(true, x);
 	}
 	
-	@Test
-	public void test() 
+@Test
+	public void TC02() 
 	{
-		fail("Not yet implemented");
+		//Skrevet af: Kasper Leiszner
+		Dice dc = new Dice();
+		
+		int countOfOne = 0;
+		int countOfTwo = 0;
+		int countOfThree = 0;
+		int countOfFour = 0;
+		int countOfFive = 0;
+		int countOfSix = 0;
+
+		for (int i = 0; i < 60000; i++) 
+		{
+			switch(dc.roll())
+			{
+			case 1:
+				countOfOne++;
+				break;
+			case 2:
+				countOfTwo++;
+				break;
+			case 3:
+				countOfThree++;
+				break;
+			case 4:
+				countOfFour++;
+				break;
+			case 5:
+				countOfFive++;
+				break;
+			case 6:
+				countOfSix++;
+				break;
+			}
+		}
+		
+		assertEquals(countOfOne, 10000, 400);
+		assertEquals(countOfTwo, 10000, 400);
+		assertEquals(countOfThree, 10000, 400);
+		assertEquals(countOfFour, 10000, 400);
+		assertEquals(countOfFive, 10000, 400);
+		assertEquals(countOfSix, 10000, 400);
 	}
 }
